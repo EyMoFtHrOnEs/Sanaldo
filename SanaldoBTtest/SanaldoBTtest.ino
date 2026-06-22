cc350c139d20b4ea1936e7d9ca970fd8f96074d6
HamzaYslmn <Hamzayesilmen7@gmail.com>
2026-06-22 22:32:26 +0300

    yeni test

--- SanaldoBTtest/SanaldoBTtest.ino
+++ SanaldoBTtest/SanaldoBTtest.ino
@@ -12,7 +12,13 @@ BluetoothSerial SerialBT;
 
 void setup() {
   Serial.begin(115200);
-  SerialBT.begin("SanaldoBT");
+  delay(500);
+  bool ok = SerialBT.begin("SanaldoBT");
+  Serial.printf("[BOOT] SerialBT.begin = %s\n", ok ? "OK" : "FAIL");
+  uint8_t mac[6];
+  esp_read_mac(mac, ESP_MAC_BT);
+  Serial.printf("[BOOT] BT MAC = %02X:%02X:%02X:%02X:%02X:%02X\n",
+                mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
   Serial.println(F("[BOOT] Pair phone to 'SanaldoBT', then send text from a BT terminal app."));
 }
 
